fd8169ca6fefffea4583f8d6d90f425177f357fe
L6Z6 <2992856804@qq.com>
2023-09-08 23:18:45 +0800

    撤回axios封装。重复请求暂时未处理

--- src/components/AlbumCard/index.vue
+++ src/components/AlbumCard/index.vue
@@ -9,10 +9,8 @@
 </template>
 
 <script setup lang="ts">
-import { ElMessage } from 'element-plus'
-
+import { ElMessage} from 'element-plus/es'
 const prop = defineProps(['list'])
-
 </script>
 
 <style scoped>
